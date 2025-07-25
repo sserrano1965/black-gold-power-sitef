68056a1976937b0088983a09e26667cae830bb11
sserrano1965 <salvador_serrano132@hotmail.com>
2025-07-25 17:59:37 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+document.getElementById('languageSwitcher').addEventListener('change', function() {
+  const lang = this.value;
+  document.querySelectorAll('[data-en]').forEach(el => {
+    el.textContent = el.getAttribute('data-' + lang);
+  });
+});
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const sectionId = this.getAttribute('data-section');
+    document.querySelectorAll('.content-section').forEach(sec => {
+      sec.classList.remove('visible');
+    });
+    document.getElementById(sectionId).classList.add('visible');
+  });
+});
